21dd1794ef16a5d704cf142ba59aae63c04a52e8
Ranjeet225 <ranjeetmaurya2033@gmail.com>
2025-11-08 14:59:15 +0530

    data

--- application/views/templates/accountant_header.php
+++ application/views/templates/accountant_header.php
@@ -199,6 +199,7 @@
                <li> <a href="<?php echo base_url(); ?>accounts/patient_center_wise_report" class="waves-effect waves-dark"><i class="fa fa-sitemap"></i>Patient Journey</a></li>
                <li> <a href="<?php echo base_url(); ?>accounts/revenue_potential" class="waves-effect waves-dark"><i class="fa fa-sitemap"></i>Revenue Potential</a></li>
                 <li><a href="<?php echo base_url(); ?>my_appointments" class="waves-effect waves-dark"><i class="fa fa-sitemap"></i>My appointments</a></li>
+                <li><a href="<?php echo base_url(); ?>patients/timeline_view" class="waves-effect waves-dark"><i class="fa fa-sitemap"></i>Patient Agent</a></li>
             </ul>
             <?php } ?>
          </div>
