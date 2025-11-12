fc990d0451ca2737b7c17f84b89c59fb9b2d1fac
Ranjeet225 <ranjeetmaurya2033@gmail.com>
2025-11-12 12:49:15 +0530

    data

--- application/controllers/Billingcontroller.php
+++ application/controllers/Billingcontroller.php
@@ -2811,6 +2811,7 @@ function partial_billing($appointment_id){
 
 	public function search_doctor_in_Camp()
     {
+		die;
         $centre_id = $this->input->post('centre_id');
         if (empty($centre_id)) {
             echo '<option value="">Invalid Centre</option>';
