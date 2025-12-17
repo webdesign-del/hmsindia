2ba949f08276a31c40f1bb64a949e95bc42b441a
Ranjeet225 <ranjeetmaurya2033@gmail.com>
2025-12-17 15:47:17 +0530

    data

--- application/views/stocks_new/edit_sale.php
+++ application/views/stocks_new/edit_sale.php
@@ -476,9 +476,9 @@
                                 <span class="badge badge-success">Sale confirmed and stock reduced using FEFO</span>
                             <?php endif; ?>
                             
-                            <a href="<?php echo base_url('stocks_new/print_sale/' . $sale->id); ?>" target="_blank" class="btn btn-info">
+                            <!-- <a href="<?php echo base_url('stocks_new/print_sale/' . $sale->id); ?>" target="_blank" class="btn btn-info">
                                 <i class="fa fa-print"></i> Print Bill
-                            </a>
+                            </a> -->
                             
                             <a href="<?php echo base_url('stocks_new/sales'); ?>" class="btn btn-default">
                                 <i class="fa fa-arrow-left"></i> Back to Sales
