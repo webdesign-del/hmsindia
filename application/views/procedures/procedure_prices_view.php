b74d575b97adbe602fca9f45a9d53c6f8051cfbd
ghanshyam <webdesign@indiaivf.in>
2026-04-24 15:49:24 +0530

    procedure update

--- application/views/procedures/procedure_prices_view.php
+++ application/views/procedures/procedure_prices_view.php
@@ -1,12 +1,16 @@
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+
 <form action="<?php echo base_url('procedures/save_procedure_price'); ?>" method="post">
     <div class="row">
         <div class="col-md-4">
-            <label>Select Master Procedure</label>
-            <select name="procedure_id" id="proc_id" class="form-control" required onchange="fillMetadata()">
-                <option value="">-- Choose Procedure --</option>
+            <label>Select Master Procedure (Searchable)</label>
+            <select name="procedure_id" id="proc_id" class="form-control select2" required onchange="fillMetadata()">
+                <option value="">-- Type to Search --</option>
                 <?php foreach($master_procedures as $m): ?>
-                    <option value="<?= $m->ID; ?>" data-name="<?= $m->procedure_name; ?>" data-code="<?= $m->code; ?>">
-                        <?= $m->procedure_name; ?> (<?= $m->code; ?>)
+                    <option value="<?= $m->ID; ?>" 
+                            data-name="<?= $m->procedure_name; ?>" 
+                            data-code="<?= $m->code; ?>"
+                            data-price="<?= $m->price; ?>"> <?= $m->procedure_name; ?> (<?= $m->code; ?>)
                     </option>
                 <?php endforeach; ?>
             </select>
@@ -17,11 +21,11 @@
 
         <div class="col-md-3">
             <label>Min Price</label>
-            <input type="number" name="min_price" class="form-control" required step="0.01">
+            <input type="number" name="min_price" id="min_price" class="form-control" required step="0.01">
         </div>
         <div class="col-md-3">
             <label>Actual Price</label>
-            <input type="number" name="actual_price" class="form-control" required step="0.01">
+            <input type="number" name="actual_price" id="actual_price" class="form-control" required step="0.01">
         </div>
         <div class="col-md-2">
             <label>&nbsp;</label>
@@ -30,13 +34,37 @@
     </div>
 </form>
 
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
 <script>
+$(document).ready(function() {
+    // Initialize Searchable Dropdown
+    $('.select2').select2({
+        placeholder: "Type procedure name...",
+        allowClear: true,
+        width: '100%'
+    });
+
+    // Trigger metadata fill when Select2 changes
+    $('#proc_id').on('select2:select', function (e) {
+        fillMetadata();
+    });
+});
+
 function fillMetadata() {
     var select = document.getElementById('proc_id');
     var selected = select.options[select.selectedIndex];
     
-    // Pull the data- attributes we set in the foreach loop
+    // 1. Fill Hidden Fields
     document.getElementById('hidden_name').value = selected.getAttribute('data-name');
     document.getElementById('hidden_code').value = selected.getAttribute('data-code');
+    
+    // 2. Auto-load the Actual Price from the Master Table
+    var masterPrice = selected.getAttribute('data-price');
+    document.getElementById('actual_price').value = masterPrice;
+    
+    // Optional: Set Min Price to the same value as a starting point
+    document.getElementById('min_price').value = masterPrice;
 }
 </script>
