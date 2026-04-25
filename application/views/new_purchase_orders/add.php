5afc418e8c1cb86297f53f40561c1ac0c44f5c84
ghanshyam <webdesign@indiaivf.in>
2026-04-25 15:35:11 +0530

    add purchase

--- application/views/new_purchase_orders/add.php
+++ application/views/new_purchase_orders/add.php
@@ -109,6 +109,9 @@
                                 <option value="Embryology Basant Lok">Embryology Basant Lok</option>
                                 <option value="Embryology Srinagar">Embryology Srinagar</option>
                                 <option value="OT Srinagar">OT Srinagar</option>
+                                 <option value="Embryology">Embryology</option>
+                                <option value="OT">OT</option>
+                                <option value="Hormonal">Hormonal</option>
                                 <option value="Stationary And HouseKeeping">Stationary And HouseKeeping</option>
                             </select>
                         </div>
