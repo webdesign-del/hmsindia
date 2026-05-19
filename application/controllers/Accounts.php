7c5762a6c8d5e251ef0967cb408d26856211e211
ghanshyam <webdesign@indiaivf.in>
2026-05-19 11:14:48 +0530

    investigation update

--- application/controllers/Accounts.php
+++ application/controllers/Accounts.php
@@ -716,7 +716,7 @@ class Accounts extends CI_Controller {
 				header('Content-Type: text/csv; charset=utf-8');
 				header('Content-Disposition: attachment; filename=Investigation-Patients-'.$start_date.'-'.$end_date.'.csv');
 				$fp = fopen('php://output','w');
-				$headers = 'IIC ID, Patient Name, Total package, Discounted Package, Paid Amount, Remaining Amount, Payment Method, Billing From, Billing At, Billing Type, Date, Status, Origins';
+				$headers = 'IIC ID, Patient Name, Receipt Number, Total package, Discounted Package, Paid Amount, Remaining Amount, Payment Method, Billing From, Billing At, Billing Type, Date, Status, Origins';
 				//Add the headers
 				fwrite($fp, $headers. "\r\n");
 				foreach ($data as $key => $val) {//var_dump($val);die;
@@ -732,7 +732,7 @@ class Accounts extends CI_Controller {
 					
 					$billing_at = get_center_name($val['billing_at']);
 					
-					$lead_arr = array($val['patient_id'], $val['wife_name'], $val['totalpackage'], $val['discounted_package'], $val['payment_done'], $val['remaining_amount'], $val['payment_method'], $billing_from, $billing_at, $val['billing_type'], date('Y-m-d H:i:s', strtotime($val['date'])), $val['status'],$origin_from);
+					$lead_arr = array($val['patient_id'], $val['wife_name'], $val['receipt_number'], $val['totalpackage'], $val['discounted_package'], $val['payment_done'], $val['remaining_amount'], $val['payment_method'], $billing_from, $billing_at, $val['billing_type'], date('Y-m-d H:i:s', strtotime($val['date'])), $val['status'],$origin_from);
 					fputcsv($fp, $lead_arr);
 				}
 				fclose($fp);
