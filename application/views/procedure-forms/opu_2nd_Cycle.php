6224929fa389a10d994a24d7cc37b6b8c4b7ca7d
ghanshyam <webdesign@indiaivf.in>
2026-08-10 14:07:58 +0530

    opu

--- application/views/procedure-forms/opu_2nd_Cycle.php
+++ application/views/procedure-forms/opu_2nd_Cycle.php
@@ -3,7 +3,7 @@
     if(isset($_POST['submit'])){
         unset($_POST['submit']);
         //var_dump($_POST);die;
-		$select_query = "SELECT * FROM `hms_opu` WHERE patient_id='$patient_id' and receipt_number='$receipt_number'";
+		$select_query = "SELECT * FROM `hms_opu` WHERE patient_id='$patient_id' and receipt_number='$receipt_number' and type='Second Cycle'";
         $select_result = run_select_query($select_query); 
         if(empty($select_result)){
             // mysql query to insert data
@@ -34,19 +34,16 @@
 					die();
         }
     }
-	$select_query = "SELECT * FROM `hms_opu` WHERE patient_id='$patient_id' and receipt_number='$receipt_number'";
+	$select_query = "SELECT * FROM `hms_opu` WHERE patient_id='$patient_id' and receipt_number='$receipt_number' and type='Second Cycle'";
     $select_result = run_select_query($select_query);
 	
 	$sql3 = "SELECT * FROM `hms_patients` WHERE patient_id='$patient_id'";
     $select_result3 = run_select_query($sql3); 	
 	
-	$sql1 = "Select * from ".$this->config->item('db_prefix')."appointments where paitent_id='".$patient_id."'";
+	$sql1 = "Select * from ".$this->config->item('db_prefix')."appointments where paitent_id='".$patient_id."' and paitent_type='new_patient'";
 	$select_result1 = run_select_query($sql1);
 	
-	$sql4 = "Select * from ".$this->config->item('db_prefix')."appointments where wife_phone='".$select_result1['wife_phone']."' and paitent_type='new_patient'";
-	$select_result4 = run_select_query($sql4);
-	
-	$sql5 = "Select * from ".$this->config->item('db_prefix')."centers where center_number='".$select_result4['appoitment_for']."'";
+	$sql5 = "Select * from ".$this->config->item('db_prefix')."centers where center_number='".$select_result1['appoitment_for']."'";
 	$select_result5 = run_select_query($sql5);
 ?>
 
@@ -55,7 +52,7 @@
 <input type="hidden" value="<?php echo $updated_by; ?>" class="form" name="updated_by">
 <input type="hidden" value="<?php echo $updated_type; ?>" class="form" name="updated_type">
 <input type="hidden" value="<?php echo $updated_at; ?>" class="form" name="updated_at">
-
+<input type="hidden" value="Second Cycle" class="form" name="type">
     <input type="hidden" value="<?php echo $procedure_id; ?>" class="form" name="procedure_id">
 	<input type="hidden" value="<?php echo $patient_id; ?>" class="form" name="patient_id">
 	<input type="hidden" value="<?php echo $receipt_number; ?>" class="form" name="receipt_number">
@@ -72,7 +69,7 @@
 <tr style="background: #b3b9b7;">
 
 <td colspan="2" width="33%" style="border:1px solid;padding:5px;">
-<strong>UHID : <?php echo $select_result5['center_code']."/".$select_result4['uhid']; ?></strong>
+<strong>UHID : <?php echo $select_result5['center_code']."/".$select_result1['uhid']; ?></strong>
 </td>
 <td colspan="2" width="33%" style="border:1px solid;padding:5px;">
 <strong>Patient Name : <?php echo $select_result3['wife_name']; ?> </strong>
@@ -367,7 +364,7 @@
 <tr style="background: #b3b9b7;">
 
 <td colspan="2" width="33%" style="border:1px solid;padding:5px;">
-<strong>UHID : <?php echo $select_result5['center_code']."/".$select_result4['uhid']; ?></strong>
+<strong>UHID : <?php echo $select_result5['center_code']."/".$select_result1['uhid']; ?></strong>
 </td>
 <td colspan="2" width="33%" style="border:1px solid;padding:5px;">
 <strong>Patient Name : <?php echo $select_result3['wife_name']; ?> </strong>
